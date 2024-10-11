43b9e35894359f0b43bf43f22c116985ec9bcc0a
bruno-corso <brunotcorso@gmail.com>
2024-10-11 17:01:29 -0300

    criando model e controller visitas

---
+++ src/controllers/visitasController.js
@@ -0,0 +1,50 @@
+import visitasModel from "../models/visitasModel.js";
+
+export const criarVisita = async (req, res) => {
+  try {
+    const {
+      visitor_name,
+      visitor_photoUrl,
+      visit_reason,
+      visit_email,
+      visit_phone,
+      visit_car_model,
+      visit_car_id,
+      checkInTime,
+      checkOutTime,
+      host_name,
+      host_department,
+    } = req.body;
+
+    if (
+      visitor_name ||
+      visitor_photoUrl ||
+      visit_reason ||
+      visit_email ||
+      visit_phone ||
+      visit_car_model ||
+      visit_car_id ||
+      checkInTime ||
+      host_name ||
+      host_department
+    )
+      return res
+        .status(400)
+        .json({ message: "Não foi preenchido todos os campos" });
+
+    const novaVisita = new visitasModel({
+      visitor_name,
+      visitor_photoUrl,
+      visit_reason,
+      checkInTime,
+      checkOutTime,
+      host_name,
+      host_department,
+      visit_email,
+      visit_phone,
+      visit_car_model,
+      visit_car_id,
+      user_register: req.user.name
+    });
+  } catch (error) {}
+};
